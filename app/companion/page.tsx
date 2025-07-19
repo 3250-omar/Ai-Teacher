6ae28111ac9a81b7e205ec25663fcc4d4e5b3a12
omar mostafa <om083418@gmail.com>
2025-07-19 13:52:21 +0300

    Fix Build 2

--- app/companion/page.tsx
+++ app/companion/page.tsx
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import CompanionCard from "@/components/CompanionCard";
 import FilterSubject from "@/components/FilterSubject";
 import SearchInput from "@/components/SearchInput";
